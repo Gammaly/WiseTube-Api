89cdfc743caa0c7cc6dd67f19ba1c3b940f3d3bc
NianHung <joanne.lee@iss.nthu.edu.tw>
2022-04-23 15:01:57 +0800

    add db/migration

---
+++ app/db/migrations/002_playlists_create.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:playlists) do
+      primary_key :id
+      foreign_key :owner_id, :accounts
+
+      String :name, unique: true, null: false
+      String :playlist_url, unique: true
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
