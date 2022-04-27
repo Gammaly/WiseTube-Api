f05803957324303629c14efa39b724a1874b4ecf
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2022-04-27 10:58:27 +0800

    Update app.rb based on commit

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -40,12 +40,11 @@ module WiseTube
 
             response.status = 201
             response['Location'] = "#{@account_route}/#{new_account.id}"
-            { message: 'Playlist saved', data: new_account }.to_json
+            { message: 'Account created', data: new_account }.to_json
           rescue Sequel::MassAssignmentRestriction
             routing.halt 400, { message: 'Illegal Request' }.to_json
           rescue StandardError => e
-            puts e.inspect
-            routing.halt 500, { message: error.message }.to_json
+            routing.halt 500, { message: e.message }.to_json
           end
         end
 
