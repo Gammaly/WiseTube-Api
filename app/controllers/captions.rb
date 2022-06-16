32767b6bdf6d3e52a002eb714cb2ac3527cfb6ab
schwarz-tw <schwarz.tsai@gmail.com>
2022-06-16 11:34:49 +0800

    Add captions

---
+++ app/controllers/captions.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative './app'
+
+module WiseTube
+  # Web controller for WiseTube API
+  class Api < Roda
+    route('captions') do |routing|
+      #routing.halt 403, { message: 'Not authorized' }.to_json unless @auth_account
+
+      @link_route = "#{@api_root}/captions"
+
+      # GET api/v1/captions?q=keyword
+      routing.on do
+        q = routing.params["q"]
+        routing.get do
+          data = GetCaptions.call(q: q)
+
+          { data: data }.to_json
+        rescue GetCaptions::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetCaptions::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          Api.logger.warn "Search Error: #{e.inspect}"
+          routing.halt 500, { message: "API server error #{e.inspect}" }.to_json
+        end
+      end
+    end
+  end
+end
---
+++ app/services/get_captions.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module WiseTube
+  # Search videos
+  class GetCaptions
+    # Error for cannot search video
+    class ForbiddenError < StandardError
+      def message
+        'You are not allowed to access that caption'
+      end
+    end
+
+    # Error for cannot find a video
+    class NotFoundError < StandardError
+      def message
+        'We could not find that caption'
+      end
+    end
+
+    # Get caption
+    def self.call(q: )
+      raise NotFoundError unless q
+
+      data = Python::Command.new.captions(q).call
+      #raise ForbiddenError unless policy.can_view?
+
+      data
+    end
+  end
+end
