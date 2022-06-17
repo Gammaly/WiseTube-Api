78a3beded5b8314cfa38f0536449aac35c9b67ae
NianHung <joanne.lee@iss.nthu.edu.tw>
2022-06-17 12:07:41 +0800

    add accounts.rb and api_accounts_spec.rb signed request

--- app/controllers/accounts.rb
+++ app/controllers/accounts.rb
@@ -28,15 +28,8 @@ module WiseTube
 
       # POST api/v1/accounts
       routing.post do
-        # YES_SignedRequest
-        # account_data = SignedRequest.new(Api.config).parse(request.body.read)
-        # new_account = Account.create(account_data)
-
-        # NO_SignedRequest
-        account_data = JSON.parse(routing.body.read)
-        new_account = Account.new(account_data)
-        raise('Could not save account') unless new_account.save
-        # END
+        account_data = SignedRequest.new(Api.config).parse(request.body.read)
+        new_account = Account.create(account_data)
 
         response.status = 201
         response['Location'] = "#{@account_route}/#{new_account.username}"
